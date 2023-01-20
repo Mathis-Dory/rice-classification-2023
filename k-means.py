ce5c7d601cd83b4daa8c4487418b70990faf6198
mathisdory <mathisdory@gmail.com>
2023-01-20 22:22:01 +0200

    feat(kmeans): plot results

--- k-means.py
+++ k-means.py
@@ -1,143 +1,112 @@
-import os
 import ssl
 import sys
 
 import numpy as np
-import pandas as pd
-import seaborn as sb
-from keras.applications import ResNet50
-from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 
 # Do nut trunc print
-from PIL import Image
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.decomposition import PCA
-from sklearn.metrics import calinski_harabasz_score, silhouette_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.utils import shuffle
+
+from utils import load_data, show_samples
 
 np.set_printoptions(threshold=sys.maxsize)
 # Following line because import of VGG16 failed if I do not use it
 # https://stackoverflow.com/questions/47231408/downloading-resnet50-in-keras-generates-ssl-certificate-verify-failed
 ssl._create_default_https_context = ssl._create_unverified_context
+plt.rcParams.update({"font.size": 17})
 
 
 def main():
-    data_path = PrepareDataset("./dataset/")
-    data = data_path.load()
-    data_path.show_samples(data)
-    features = data_path.exctract_features(data)
-    features = data_path.preprocess_features(features)
-    x_train, x_test, y_train, y_test = data_path.split(features, data)
-    kmean = KmeansModel(x_train, x_test, y_train, y_test)
-    trained_kmean = kmean.train_model(5)
-    kmean.predict_evaluate(trained_kmean)
+    dataset = PrepareDataset("./dataset/")
+    x, y, df = load_data()
+    show_samples(df)
+    x_train, x_test, y_train, y_test = dataset.split(x, y)
+    x_train_features, x_test_features, process = dataset.exctract_features(
+        x_train, x_test, "pca"
+    )
+    x_test_indexes = dataset.x_test_indexes
+    kmean = KmeansModel(
+        x_train_features,
+        x_test_features,
+        y_train,
+        y_test,
+        df["filepath"],
+        x_test_indexes,
+    )
+    # I chose 5 because I already Know I have 5 labels
+    kmean.fit_predict(5, process)
 
 
 class PrepareDataset:
     def __init__(self, path_dataset_folder):
         self.data_folder = path_dataset_folder
-        self.model_resnet = ResNet50(
-            weights="imagenet",
-            include_top=False,
-        )
 
-    def load(self):
-        images_list = []
-        labels = []
-        # Get all folders available
-        for item in os.scandir(self.data_folder):
-            # Take only folders (because my images are splitted into 5 different folders)
-            if item.is_dir():
-                # Foreach image
-                for image in os.scandir(f"{self.data_folder}/{item.name}"):
-                    # Add the label of the image
-                    labels.append(item.name)
-                    # Prepare a dataframe with the filepath of the images
-                    images_list.append(image.path)
-        df = pd.DataFrame()
-        df["filepath"] = images_list
-        df["label"] = labels
-        df = shuffle(df)
-        df.reset_index(drop=True, inplace=True)
-        df.head(5)
-        # Display proportion of labels
-        sb.countplot(x=labels)
-        plt.show()
-        return df
-
-    def show_samples(self, df):
-        plt.figure(figsize=(10, 10))
-        for i, _ in enumerate(range(1, 16), start=1):
-            if i in [1, 6, 11]:
-                label = "Karacadag"
-            elif i in [2, 7, 12]:
-                label = "Ipsala"
-            elif i in [3, 8, 13]:
-                label = "Basmati"
-            elif i in [4, 9, 14]:
-                label = "Arborio"
-            else:
-                label = "Jasmine"
-            plt.subplot(3, 5, i)
-            # Take random sample corresponding to a specific label
-            random_row = df[df["label"] == label].sample()
-            sample = Image.open(random_row["filepath"].values[0])
-            plt.title(label)
-            plt.imshow(sample)
-        plt.show()
-
-    def exctract_features(self, df):
-        generator = ImageDataGenerator(
-            rescale=1.0 / 255,
-        )
-        data = generator.flow_from_dataframe(
-            dataframe=df,
-            target_size=(64, 64),
-            batch_size=64,
-            x_col="filepath",
-            y_col="label",
-            class_mode=None,
-            shuffle=False,
-        )
-        features = self.model_resnet.predict(data)
-        features = features.reshape(features.shape[0], -1)
-        return features
-
-    def preprocess_features(self, features):
-        scaler = MinMaxScaler()
-        features = scaler.fit_transform(features)
-        pca = PCA(n_components=100)
-        features = pca.fit_transform(features)
-        return features
-
-    def split(self, features, df):
-        x_train, x_test, y_train, y_test = train_test_split(
-            features, df["label"], test_size=0.3
-        )
+    def split(self, x, y):
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+        self.x_test_indexes = [i for i, x in enumerate(x_test)]
         return x_train, x_test, y_train, y_test
+
+    def exctract_features(self, x_train, x_test, process):
+        if process == "pca":
+
+            pca = PCA(n_components=100)
+            x_train = x_train.reshape(
+                -1, x_train.shape[1] * x_train.shape[2] * x_train.shape[3]
+            )
+
+            x_test = x_test.reshape(
+                -1, x_test.shape[1] * x_test.shape[2] * x_test.shape[3]
+            )
+            pca.fit(x_train)
+            x_train = pca.transform(x_train)
+            x_test = pca.transform(x_test)
+            return x_train, x_test, pca
 
 
 class KmeansModel:
-    def __init__(self, x_train, x_test, y_train, y_test):
-        self.x_train = x_train
-        self.x_test = x_test
+    def __init__(
+        self,
+        x_train_features,
+        x_test_features,
+        y_train,
+        y_test,
+        image_paths,
+        x_test_indexes,
+    ):
+        self.x_train = x_train_features
+        self.x_test = x_test_features
         self.y_train = y_train
         self.y_test = y_test
+        self.image_paths = image_paths
+        self.x_test_indexes = x_test_indexes
 
-    def train_model(self, n):
-        kmeans = MiniBatchKMeans(n_clusters=n)
-        return kmeans.fit(self.x_train)
+    def fit_predict(self, n, process):
+        kmeans = MiniBatchKMeans(n_clusters=n, batch_size=1000, random_state=42)
+        kmeans.fit(self.x_train)
+        pred_labels = kmeans.predict(self.x_test)
 
-    def predict_evaluate(self, trained_kmeans):
-        y_pred = trained_kmeans.predict(self.x_test)
+        # Create an array that contains 5 other arrays (one per cluster)
+        clusters = [[] for _ in range(n)]
+        # For each cluster append all index of pred_labels where the predicted label is equal to the current cluster
+        for i, label in enumerate(pred_labels):
+            clusters[label].append(i)
 
-        silhouette = silhouette_score(self.x_test, y_pred)
-        print("Silhouette score:", silhouette)
-        calinski = calinski_harabasz_score(self.x_test, y_pred)
-        print("Calinski_harabasz score:", calinski)
+        # for each cluster
+        # Plot the first 50 images of each cluster
+        for cluster in clusters:
+            fig, axes = plt.subplots(4, 10, figsize=(30, 30))
+            # Loop 40 first images
+            for j, idx in enumerate(cluster[:40]):
+                # Find original image among test using the index from the predicted labels we saved
+                img = plt.imread(self.image_paths[self.x_test_indexes[idx]])
+                ax = axes[j // 10, j % 10]
+                ax.imshow(img)
+                # Get the true label of the image
+                ax.set_title(self.y_test[idx])
+            fig.tight_layout()
+            plt.show()
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,95 @@
+import os
+from itertools import islice
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sb
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+from PIL import Image
+
+
+def load_data(path="./dataset"):
+    """
+    Can be used to load the dataset
+    """
+
+    X = []
+    y = []
+    images_path = []
+    # Get all folders available
+    for item in os.scandir(path):
+        # Take only folders (because my images are splitted into 5 different folders) and ignore files
+        if item.is_dir():
+            # Foreach image but take only the first 1500 because lack of memory
+            for image in islice(os.scandir(f"{path}/{item.name}"), 1000):
+                # Add the label of the image
+                if item.name == "Arborio":
+                    y.append(0)
+                elif item.name == "Basmati":
+                    y.append(1)
+                elif item.name == "Ipsala":
+                    y.append(2)
+                elif item.name == "Jasmine":
+                    y.append(3)
+                else:
+                    y.append(4)
+                images_path.append(image.path)
+                temp = plt.imread(image.path)
+                X.append(temp)
+
+    df = pd.DataFrame()
+    df["filepath"] = images_path
+    df["label"] = y
+    # normalize it
+    X = np.asarray(X).reshape(-1, 250, 250, 3) / 255.0
+    # Display proportion of labels
+    sb.countplot(x=y)
+    plt.show()
+
+    return X, np.asarray(y), df
+
+
+def show_samples(df):
+    plt.figure(figsize=(10, 10))
+    for i, _ in enumerate(range(1, 16), start=1):
+        if i in [1, 6, 11]:
+            title = "Arborio"
+            label = 0
+        elif i in [2, 7, 12]:
+            title = "Basmati"
+            label = 1
+        elif i in [3, 8, 13]:
+            title = "Ipsala"
+            label = 2
+        elif i in [4, 9, 14]:
+            label = 3
+            title = "Jasmine"
+        else:
+            title = "Karacadag"
+            label = 4
+        plt.subplot(3, 5, i)
+        # Take random sample corresponding to a specific label
+        random_row = df[df["label"] == label].sample()
+        sample = Image.open(random_row["filepath"].values[0])
+        plt.title(title)
+        plt.imshow(sample)
+    plt.show()
+
+
+def imscatter(x, y, images, ax=None, zoom=0.1):
+    if ax is None:
+        ax = plt.gca()
+
+    for i, image in enumerate(images):
+
+        im = OffsetImage(image, zoom=zoom)
+        _x, _y = np.atleast_1d(x[i], y[i])
+        artists = []
+        for x0, y0 in zip(_x, _y):
+            ab = AnnotationBbox(im, (x0, y0), xycoords="data", frameon=False)
+            artists.append(ax.add_artist(ab))
+        ax.update_datalim(np.column_stack([_x, _y]))
+        ax.autoscale()
+
+    return artists
